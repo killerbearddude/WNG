46080c7eba5ec2c4c13992f4922b9fc71adb2cfa
killerbearddude <58448059+killerbearddude@users.noreply.github.com>
2026-06-14 18:30:32 -0600

    Add graph-level history foundation

--- tests/test_public_header_policy.cpp
+++ tests/test_public_header_policy.cpp
@@ -58,6 +58,7 @@ int main()
         "include/wng/graph.hpp",
         "include/wng/graph_command.hpp",
         "include/wng/graph_command_history.hpp",
+        "include/wng/graph_history.hpp",
         "include/wng/graph_command_transaction.hpp",
         "include/wng/graph_diff.hpp",
         "include/wng/graph_mutation_preview.hpp",
