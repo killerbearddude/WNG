9084c2f8e72f6edbe3c9e4d7ad4695978464c42a
killerbearddude <58448059+killerbearddude@users.noreply.github.com>
2026-06-17 10:40:44 -0600

    Add graph hit testing to public header policy

--- tests/test_public_header_policy.cpp
+++ tests/test_public_header_policy.cpp
@@ -63,6 +63,7 @@ int main()
         "include/wng/graph_editor_state.hpp",
         "include/wng/graph_editor_state_cleanup.hpp",
         "include/wng/graph_editor_selection_commands.hpp",
+        "include/wng/graph_hit_testing.hpp",
         "include/wng/graph_command_transaction.hpp",
         "include/wng/graph_diff.hpp",
         "include/wng/graph_mutation_preview.hpp",
