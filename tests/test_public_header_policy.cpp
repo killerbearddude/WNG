b03c7254f4f67ce7b47611377309db2326916089
killerbearddude <58448059+killerbearddude@users.noreply.github.com>
2026-06-17 13:00:41 -0600

    Add pending-link header to public header policy

--- tests/test_public_header_policy.cpp
+++ tests/test_public_header_policy.cpp
@@ -65,6 +65,7 @@ int main()
         "include/wng/graph_editor_selection_commands.hpp",
         "include/wng/graph_hit_testing.hpp",
         "include/wng/graph_editor_hit_testing.hpp",
+        "include/wng/graph_editor_pending_link_state.hpp",
         "include/wng/graph_command_transaction.hpp",
         "include/wng/graph_diff.hpp",
         "include/wng/graph_mutation_preview.hpp",
